aaa0da89e270975ae99dd9e78b8e917626c8f83c
idriscahyono <idriscahyono@gmail.com>
2019-11-24 22:49:28 +0700

    fix 4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const router = require("./config/router");
 require("./config/db");
 
 app.use(express.json());
+app.use(cors());
 
 
 app.get("/", function (req, res) {
